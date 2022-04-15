fde63c9e3b352198a06801967411551454bac628
Han-Hsin Lin <hanhsin.lin@student.unsw.edu.au>
2022-04-15 21:07:47 +1000

    Add Login requests to backend

--- frontend/src/page/Login.jsx
+++ frontend/src/page/Login.jsx
@@ -4,86 +4,148 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-
 // import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { AUTH } from '../config';
+
+// Calls AUTH.LOGIN_URL to login
+// @param {Json} loginDataJson
+// @returns {Promise.Json} response body from login request
+export function requestLoginAsUser(loginDataJson) {
+  const loginRequest = new Request(AUTH.LOGIN_URL,
+      {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: loginDataJson
+      });
+
+  console.log(loginRequest);
+
+  return fetch(loginRequest)
+      .then(response => {
+          if (response.ok) {
+              return response.json();
+          } else {
+              return response.json().then(errorJson => {
+                  throw Error(`${response.status} ${response.statusText} [${errorJson["error"]}]`);
+              });
+          }
+      })
+      .then(responseObj => {
+          // console.log("requestLoginAsUser():"+ JSON.stringify(responseObj));
+          return responseObj;
+      })
+      .catch((error) => {
+          console.error('function requestLoginAsUser fetch failed', error);
+          throw error;
+      });
+
+}
+
+// Perform actions for login
+// @param {Object} formDataObj
+// @returns {Promise.Object} response body from login request
+export function loginAsUser(formDataObj) {
+  //Convert object to json
+  let loginDataJson = "";
+  loginDataJson = JSON.stringify(formDataObj);
+
+  return requestLoginAsUser(loginDataJson)
+      .then(responseObj => {
+          return responseObj;
+      }).catch(error => {
+          console.error('function loginAsUser failed', error);
+          throw(error);
+      });
+}
 
 function Login() {
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
+    const registerFD = new FormData(event.currentTarget);
+    let formDataObj = {};
+    registerFD.forEach((value, key) => formDataObj[key] = value);
+
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      formDataObj
     });
+    loginAsUser(formDataObj).then(responseObj => {
+      const CUR_USER_TOKEN = responseObj["token"];
+      console.log(CUR_USER_TOKEN);
+
+    }).catch((error) => {
+      console.error('Login failed', error);
+      alert(error);
+    });
+
   };
 
   return (
     <Container maxWidth="sm">
-              <CssBaseline />
-        <Box
+      <CssBaseline />
+      <Box
+        sx={{
+          my: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          borderRadius: 2,
+          py: 5,
+          bgcolor: '#f0f0f0',
+        }}
+      >
+        <Typography component="h1" variant="h3">
+          LOGIN
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate
           sx={{
-            my: 8,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            borderRadius: 2,
-            py: 5,
-            bgcolor: '#f0f0f0',
+            width: 0.7,
+            mt: 1,
           }}
         >
-          <Typography component="h1" variant="h3">
-            LOGIN
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              width:0.7,
-              mt: 1,
-            }}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            size="large"
+            color="success"
+            sx={{ mt: 3, mb: 2, width: 0.8, fontSize: 24 }}
           >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              size="large"
-              color="success"
-              sx={{ mt: 3, mb: 2, width: 0.8, fontSize: 24 }}
-            >
-              LOGIN
-            </Button>
+            LOGIN
+          </Button>
 
-            <Link sx = {{mt:2}} href="#" variant="body2">
-              Click Here to register
-            </Link>
-            <Link sx = {{mt:2}} href="#" variant="body2">
-              Want to Join a game? Click Here
-            </Link>
-          </Box>
+          <Link sx={{ mt: 2 }} href="#" variant="body2">
+            Click Here to register
+          </Link>
+          <Link sx={{ mt: 2 }} href="#" variant="body2">
+            Want to Join a game? Click Here
+          </Link>
         </Box>
+      </Box>
     </Container>
   );
 }
